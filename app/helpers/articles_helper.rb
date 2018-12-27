f548d97485d8b2e047244dd49dc847953d150211
Khalal Walker <khalal.walker@gmail.com>
2018-12-26 18:12:32 -0800

    committing tagging functionality

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 module ArticlesHelper
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
-
 end
